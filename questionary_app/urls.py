333ff3946a0a185bda2f97b9faa19ab74f1dff58
kim-shun <kimura1417080@gmail.com>
2023-02-11 11:15:42 +0900

    暫定的なログイン機能実装

--- questionary_app/urls.py
+++ questionary_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
+
 from . import views
 
 app_name = 'questionary_app'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name="index")
+    path('', views.IndexView.as_view(), name="index"),
 ]
